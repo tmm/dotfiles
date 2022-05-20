d538f5db9f3e2db11792b8d775fdf7d128c4c76e
Tom Meagher <tom@meagher.co>
2022-05-20 19:57:22 -0400

    feat: update colors

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,33 +1,36 @@
 local set = vim.opt
 
-set.autoindent     = true                                  -- Good auto indent
-set.clipboard      = 'unnamed,unnamedplus'                 -- Use clipboard for all operations
-set.cursorline     = true                                  -- Highlight current line
-set.expandtab      = true                                  -- Expand tabs into spaces
+set.autoindent     = true -- Good auto indent
+set.clipboard      = 'unnamed,unnamedplus' -- Use clipboard for all operations
+set.cursorline     = true -- Highlight current line
+set.expandtab      = true -- Expand tabs into spaces
 set.foldmethod     = 'syntax'
-set.foldlevelstart = 4                                     -- Some folds closed
-set.hidden         = true                                  -- Handle multiple buffers better
-set.number         = true                                  -- Show line numbers
-set.relativenumber = true                                  -- Line numbers relative to cursor
-set.scrolloff      = 4                                     -- Always show at least four lines above/below cursor
-set.shortmess      = 'Iac'                                 -- Disable start up message and abbreviate items
+set.foldlevelstart = 4 -- Some folds closed
+set.hidden         = true -- Handle multiple buffers better
+set.number         = true -- Show line numbers
+set.relativenumber = true -- Line numbers relative to cursor
+set.scrolloff      = 4 -- Always show at least four lines above/below cursor
+set.shortmess      = 'Iac' -- Disable start up message and abbreviate items
 set.showbreak      = '↪'
-set.sidescrolloff  = 4                                     -- Always show at least four columns left/right cursor
-set.signcolumn     = 'yes'                                 -- Always show the signcolumn
-set.smartcase      = true                                  -- Switch to case-sensitive search for capital letters
+set.sidescrolloff  = 4 -- Always show at least four columns left/right cursor
+set.signcolumn     = 'yes' -- Always show the signcolumn
+set.smartcase      = true -- Switch to case-sensitive search for capital letters
 set.smartindent    = true
-set.splitbelow     = true                                  -- More natural split opening
+set.splitbelow     = true -- More natural split opening
 set.splitright     = true
-set.swapfile       = false                                 -- Disable swapfiles
-set.termguicolors  = true                                  -- Support for true color (https://github.com/termstandard/colors)
-set.title          = true                                  -- Set terminal title
-set.undodir        = vim.env.XDG_CONFIG_HOME..'/nvim/undo'
+set.swapfile       = false -- Disable swapfiles
+set.termguicolors  = true -- Support for true color (https://github.com/termstandard/colors)
+set.title          = true -- Set terminal title
+set.undodir        = vim.env.XDG_CONFIG_HOME .. '/nvim/undo'
 set.undofile       = true
-set.updatetime     = 100                                   -- Faster update time
-set.visualbell     = true                                  -- Disable beeping
-set.wildmode       = 'longest:full,full'                   -- Completion settings
+set.updatetime     = 100 -- Faster update time
+set.visualbell     = true -- Disable beeping
+set.wildmode       = 'longest:full,full' -- Completion settings
 
 -- When using fish, set shell to bash
 if vim.env.SHELL:match('fish$') then
   set.shell = '/bin/bash'
 end
+
+vim.g.rasmus_variant = 'dark'
+vim.cmd([[colorscheme rasmus]])
