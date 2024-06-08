98c9c746df0a8f87758fa40abdaf7b8532bbcc10
Tom Meagher <tom@meagher.co>
2024-06-08 14:12:09 -0400

    chore: ghostty config

--- .config/nix/modules/ghostty.nix
+++ .config/nix/modules/ghostty.nix
@@ -17,7 +17,7 @@ in
     macos-non-native-fullscreen = true
     macos-option-as-alt = left
     mouse-hide-while-typing = true
-    macos-titlebar-tabs = true
+    macos-titlebar-style = tabs
     unfocused-split-opacity = 1
     window-padding-x = 0
     window-padding-y = 0
