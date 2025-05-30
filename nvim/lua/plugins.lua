71d8385b238ff70343c3f1db764dbb06aaf2f5c1
Tom Meagher <tom@meagher.co>
2025-05-30 19:48:22 -0400

    chore(nvim): remove neotest

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -237,11 +237,9 @@ return {
               return vim.bo[buf].buftype == "help"
             end,
           },
-          { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
         },
         left = {},
         right = {
-          { title = "Neotest Summary", ft = "neotest-summary", size = { width = 0.25 } },
           -- { title = "Grug Far", ft = "grug-far", size = { width = 0.4 } },
         },
         keys = {
@@ -415,8 +413,6 @@ return {
           disabled_filetypes = {
             "gitsigns-blame",
             "neo-tree",
-            "neotest-output-panel",
-            "neotest-summary",
           },
           globalstatus = false,
           icons_enabled = true,
@@ -609,171 +605,6 @@ return {
         update_n_lines = "gsn", -- Update `n_lines`
       },
     },
-  },
-
-  -- neotest (https://github.com/nvim-neotest/neotest)
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      -- adapters
-      "jfpedroza/neotest-elixir",
-      "marilari88/neotest-vitest",
-    },
-    -- stylua: ignore
-    keys = {
-      {"<leader>t", "", desc = "+test"},
-      { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
-      { "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug Nearest" },
-      { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
-      { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
-      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
-      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
-      { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
-      { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
-      { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
-      { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Toggle Watch" },
-    },
-    opts = {
-      adapters = {
-        ["neotest-elixir"] = {},
-        ["neotest-vitest"] = {
-          vitestCommand = function(file)
-            local util = require("neotest-vitest.util")
-
-            local function search_vitest_recursively(path)
-              if path == "/" then
-                return nil
-              end
-
-              local rootPath = util.find_node_modules_ancestor(path)
-              local vitestBinary = util.path.join(rootPath, "node_modules", ".bin", "vitest")
-              if util.path.exists(vitestBinary) then
-                return vitestBinary
-              end
-
-              return search_vitest_recursively(util.path.dirname(path))
-            end
-
-            return search_vitest_recursively(util.path.dirname(file))
-          end,
-          vitestConfigFile = function(file)
-            local util = require("neotest-vitest.util")
-            local filenamePattern = "{vite,vitest}.config.{js,ts}"
-            local rootPath = util.root_pattern(filenamePattern)(file)
-              or util.root_pattern("test/" .. filenamePattern)(file)
-
-            if not rootPath then
-              return nil
-            end
-
-            -- Ordered by config precedence (https://vitest.dev/config/#configuration)
-            local possibleVitestConfigNames = {
-              "vitest.config.ts",
-              "vitest.config.js",
-              "vite.config.ts",
-              "vite.config.js",
-            }
-
-            -- stylua: ignore
-            for _, configName in ipairs(possibleVitestConfigNames) do
-              local configPath = util.path.join(rootPath, configName)
-              if util.path.exists(configPath) then return configPath end
-              configPath = util.path.join(rootPath, "test", configName)
-              if util.path.exists(configPath) then return configPath end
-            end
-
-            return nil
-          end,
-        },
-      },
-      output = { open_on_run = true },
-      quickfix = {
-        open = function()
-          if require("util.init").has("trouble.nvim") then
-            require("trouble").open({ mode = "quickfix", focus = false })
-          else
-            vim.cmd("copen")
-          end
-        end,
-      },
-      status = {
-        signs = false,
-        virtual_text = true,
-      },
-    },
-    config = function(_, opts)
-      local neotest_ns = vim.api.nvim_create_namespace("neotest")
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            -- Replace newline and tab characters with space for more compact diagnostics
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
-
-      if require("util.init").has("trouble.nvim") then
-        opts.consumers = opts.consumers or {}
-        opts.consumers.trouble = function(client)
-          client.listeners.results = function(adapter_id, results, partial)
-            if partial then
-              return
-            end
-            local tree = assert(client:get_position(nil, { adapter = adapter_id }))
-
-            local failed = 0
-            for pos_id, result in pairs(results) do
-              if result.status == "failed" and tree:get_key(pos_id) then
-                failed = failed + 1
-              end
-            end
-            vim.schedule(function()
-              local trouble = require("trouble")
-              if trouble.is_open() then
-                trouble.refresh()
-                if failed == 0 then
-                  trouble.close()
-                end
-              end
-            end)
-            return {}
-          end
-        end
-      end
-
-      if opts.adapters then
-        local adapters = {}
-        for name, config in pairs(opts.adapters or {}) do
-          if type(name) == "number" then
-            if type(config) == "string" then
-              config = require(config)
-            end
-            adapters[#adapters + 1] = config
-          elseif config ~= false then
-            local adapter = require(name)
-            if type(config) == "table" and not vim.tbl_isempty(config) then
-              local meta = getmetatable(adapter)
-              if adapter.setup then
-                adapter.setup(config)
-              elseif adapter.adapter then
-                adapter.adapter(config)
-                adapter = adapter.adapter
-              elseif meta and meta.__call then
-                adapter(config)
-              else
-                error("Adapter " .. name .. " does not support setup")
-              end
-            end
-            adapters[#adapters + 1] = adapter
-          end
-        end
-        opts.adapters = adapters
-      end
-
-      require("neotest").setup(opts)
-    end,
   },
 
   -- neo-tree.nvim (https://github.com/nvim-neo-tree/neo-tree.nvim)
